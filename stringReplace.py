c0d6f68b4dc06122899dd51f13dafca34759ba20
Isaiah Weaver <iswea1@morgan.edu>
2016-11-24 22:06:19 -0500

    stringReplace file

---
+++ stringReplace.py
@@ -0,0 +1,6 @@
+#Replace all the spaces within the string with %20.  
+class Solution(object):
+	def replaceString(self,s):
+		#type s: String
+		#return type: String
+		return s.replace(" ", "%20")
